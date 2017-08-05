26cbf20ef7f57bdb92c81d54052753cfc81e7895
Igor Nagumanov <nagumanovir@yahoo.com>
2017-08-05 16:14:32 +0300

    commit

--- 2d_shapes/src/def/Main.java
+++ 2d_shapes/src/def/Main.java
@@ -2,6 +2,7 @@ package def;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.io.File;
 
 public class Main {
@@ -22,38 +23,55 @@ public class Main {
 }
 
 class ShapeDrawFrame extends JFrame {
-    private static final String APP_NAME = "Just a simple app";
+    private static final String APP_NAME = "Just a simple shape figure";
     private static final String ICON_PATH = System.getProperty("user.dir") +
             File.separator + "simple_window_text" + File.separator + "if_gallery_1646013.PNG";
 
     ShapeDrawFrame() throws HeadlessException {
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        if(kit == null)
-            throw new IllegalStateException("Toolkit is null");
-
-        Dimension dimension = kit.getScreenSize();
-        if(dimension == null || dimension.width == 0 || dimension.height == 0)
-            throw new IllegalStateException("Dimension is null");
-
-        dimension.setSize(dimension.getWidth() * 0.4, dimension.getHeight() * 0.25);
-        setSize(dimension);
-
         setName(APP_NAME);
 
         ImageIcon icon = new ImageIcon(ICON_PATH);
         setIconImage(icon.getImage());
+
+        add(new ShapeDrawComponent());
+        pack();
     }
 }
 
 class ShapeDrawComponent extends JComponent {
 
+    private Dimension dimension;
+
+    public ShapeDrawComponent() {
+        configure();
+    }
+
+    void configure() {
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        if(kit == null)
+            throw new IllegalStateException("Toolkit is null");
+
+        Dimension dimension_default = kit.getScreenSize();
+        if(dimension_default == null || dimension_default.width == 0 || dimension_default.height == 0)
+            throw new IllegalStateException("Dimension is null");
+
+        dimension = new Dimension((int)(dimension_default.getWidth() * 0.35), (int)(dimension_default.getHeight() * 0.35));
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        Graphics2D graphics2D = (Graphics2D) g;
+
+        Ellipse2D ellipse2D = new Ellipse2D.Double();
+        ellipse2D.setFrameFromDiagonal();
+
+        graphics2D.draw(ellipse2D);
+
     }
 
     @Override
     public Dimension getPreferredSize() {
-        return super.getPreferredSize();
+        return (Dimension)dimension.clone();
     }
 }
